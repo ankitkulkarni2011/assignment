6ef267f8ffc8bd3bfe24aedf44ee5bb2c389b6f1
vishnu <vishnu.bhand@iauro.com>
2019-04-11 18:36:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require ('express');
+const bodyParser = require("body-parser");
+const app = express();
+const getPrimaryKey = require('./db');
+app.use(bodyParser.json());
+const path = require('path');
+
+const db = require("./db");
+const collection = "todo";
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+
+
+app.get('/getTodos',(req,res)=>{
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+        console.log(err);
+        else
+        console.log(documents);
+        res.json(documents);
+    })
+});
+
+
+app.post('/',(req,res)=>{
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+        if (err)
+        console.log(err);
+        else
+        res.json({result : result, document : result.ops[0]});
+    });
+});
+app.delete('/:id',(req,res)=>{
+    const todoID=req.params.id;
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
+        if(err)
+        console.log(err);
+        else
+        res.send(result);
+
+    });
+});
+
+
+app.put('/:id',(req,res)=>{
+    const todoID = req.params.id;
+    const userInput = req.body;
+    db.getDB().collection(collection).findOneAndUpdate({__id : db.getPrimaryKey(todoID)},{$set :{todo :userInput.todo}},{returnOriginal :false},(err,result)=>{
+        if(err)
+        console.log(err);
+        else
+        res.json(result);
+    })
+});
+
+
+
+db.connect((err)=>{
+    if(err){
+    console.log('unable to connect to database');
+    process.exit(1);
+    }
+    else{
+        app.listen(2000,()=>{
+            console.log('connected to database,app listening on 3000');
+        });
+    }
+})
